7fed52631596ab626cde7994bba0d3c1cfb677ec
Chayanit Kaensan <Chayanit.K@augmentis.biz>
2016-09-27 12:09:49 +0700

    Edit search

--- app/src/main/java/com/augmentis/ayp/minemap/MapActivity.java
+++ app/src/main/java/com/augmentis/ayp/minemap/MapActivity.java
@@ -1,21 +1,29 @@
 package com.augmentis.ayp.minemap;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -26,10 +34,14 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
@@ -49,6 +61,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private double lat;
     private double lng;
     private Marker marker;
+    private String mSearchKey;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +78,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
     }
 
     /**
@@ -87,12 +109,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         mGoogleApiClient.connect();
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
             }
             return;
         }
@@ -129,9 +151,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             return;
         }
@@ -193,6 +215,33 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_item, menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_search);
+        final SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQuery(mSearchKey, false);//
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "Query text submitted: " + query);
+                mSearchKey = query;
+                onSearch();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, "Query text changing: " + newText);
+                return false;
+            }
+
+
+        });
+
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchView.setQuery(mSearchKey, false);
+            }
+        });
         return true;
     }
 
@@ -204,5 +253,145 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onBackPressed() {
+    }
+
+    public void onSearch() {
+
+        List<Address> addressList = null;
+
+        if (mSearchKey != null || !mSearchKey.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(mSearchKey, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(latLng).zoom(18).build();
+
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+    }
+
+    public void addMarkerToGoogleMap(int type, double lat, double lng) {
+
+        MarkerOptions options = new MarkerOptions();
+        options.position(new LatLng(lat, lng));
+        options.draggable(true).visible(true);
+
+        switch (type) {
+            case 1:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_airplane));
+                break;
+            case 2:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_bank));
+                break;
+            case 3:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_car));
+                break;
+            case 4:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_car_park));
+                break;
+            case 5:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_cinema));
+                break;
+            case 6:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_coffee));
+                break;
+            case 7:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_dessert));
+                break;
+            case 8:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_fitness));
+                break;
+            case 9:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_gas_station));
+                break;
+            case 10:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_goverment));
+                break;
+            case 11:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_home));
+                break;
+            case 12:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_hospital));
+                break;
+            case 13:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_hotel));
+                break;
+            case 14:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_mall));
+                break;
+            case 15:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_mountain));
+                break;
+            case 16:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_other));
+                break;
+            case 17:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_park));
+                break;
+            case 18:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_restaurant));
+                break;
+            case 19:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_school));
+                break;
+            case 20:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_sea));
+                break;
+            case 21:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_ship));
+                break;
+            case 22:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_temple));
+                break;
+            case 23:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_train));
+                break;
+            case 24:
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_work));
+                break;
+        }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Map Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
