ddb2ea0f340ec5fc94b68a2aa2fa62a9268cc3d0
Chayanit Kaensan <Chayanit.K@augmentis.biz>
2016-10-18 14:24:07 +0700

    Project Done! V1.0 18/10/2016

--- app/src/main/java/com/augmentis/ayp/minemap/WelcomeActivity.java
+++ app/src/main/java/com/augmentis/ayp/minemap/WelcomeActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
